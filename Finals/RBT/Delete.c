954192c89d7c9e3e0832a1d87837fba47c7e9e85
Adam Matteo Iulian <144601577+johndonneUZH@users.noreply.github.com>
2024-06-08 22:00:21 +0200

    Create Delete.c

---
+++ Finals/RBT/Delete.c
@@ -0,0 +1,117 @@
+void fixViolation(Node** root, Node* x) {
+    while (x != *root && x->color == BLACK) {
+        if (x == x->parent->left) {
+            Node* w = x->parent->right;
+            if (w->color == RED) {
+                w->color = BLACK;
+                x->parent->color = RED;
+                leftRotate(root, x->parent);
+                w = x->parent->right;
+            }
+            if (w->left->color == BLACK && w->right->color == BLACK) {
+                w->color = RED;
+                x = x->parent;
+            } else {
+                if (w->right->color == BLACK) {
+                    w->left->color = BLACK;
+                    w->color = RED;
+                    rightRotate(root, w);
+                    w = x->parent->right;
+                }
+                w->color = x->parent->color;
+                x->parent->color = BLACK;
+                w->right->color = BLACK;
+                leftRotate(root, x->parent);
+                x = *root;
+            }
+        } else {
+            Node* w = x->parent->left;
+            if (w->color == RED) {
+                w->color = BLACK;
+                x->parent->color = RED;
+                rightRotate(root, x->parent);
+                w = x->parent->left;
+            }
+            if (w->right->color == BLACK && w->left->color == BLACK) {
+                w->color = RED;
+                x = x->parent;
+            } else {
+                if (w->left->color == BLACK) {
+                    w->right->color = BLACK;
+                    w->color = RED;
+                    leftRotate(root, w);
+                    w = x->parent->left;
+                }
+                w->color = x->parent->color;
+                x->parent->color = BLACK;
+                w->left->color = BLACK;
+                rightRotate(root, x->parent);
+                x = *root;
+            }
+        }
+    }
+    x->color = BLACK;
+}
+
+Node* minimum(Node* node) {
+    while (node->left != NULL) {
+        node = node->left;
+    }
+    return node;
+}
+
+void rbTransplant(Node** root, Node* u, Node* v) {
+    if (u->parent == NULL) {
+        *root = v;
+    } else if (u == u->parent->left) {
+        u->parent->left = v;
+    } else {
+        u->parent->right = v;
+    }
+    if (v != NULL) {
+        v->parent = u->parent;
+    }
+}
+
+void deleteNode(Node** root, Node* z) {
+    Node* y = z;
+    Node* x;
+    Color yOriginalColor = y->color;
+
+    if (z->left == NULL) {
+        x = z->right;
+        rbTransplant(root, z, z->right);
+    } else if (z->right == NULL) {
+        x = z->left;
+        rbTransplant(root, z, z->left);
+    } else {
+        y = minimum(z->right);
+        yOriginalColor = y->color;
+        x = y->right;
+        if (y->parent == z) {
+            if (x != NULL) {
+                x->parent = y;
+            }
+        } else {
+            rbTransplant(root, y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        rbTransplant(root, z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
+    }
+    if (yOriginalColor == BLACK) {
+        fixViolation(root, x);
+    }
+}
+
+void rbDelete(Node** root, int data) {
+    Node* z = search(*root, data);
+    if (z == NULL) {
+        printf("Node to be deleted not found!\n");
+        return;
+    }
+    deleteNode(root, z);
+}
