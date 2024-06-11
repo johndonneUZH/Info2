b889e46d1d05eacb887ec964726f2e174c7c61d6
Adam Matteo Iulian <144601577+johndonneUZH@users.noreply.github.com>
2024-06-11 16:26:59 +0200

    Update quickSort.c

--- Finals/SORTING/quickSort.c
+++ Finals/SORTING/quickSort.c
@@ -5,36 +5,61 @@ void Swap(int* A, int x, int y)
     A[y] = t;
 }
 
-int hoarePartition(int* A, int l, int r)
-{
+int hoarePartitionLeft(int* A, int l, int r) {
     int pivot = A[l];
-    int i = l+1;
-    int j = r;
+    int i = l - 1;
+    int j = r + 1;
 
-    while (i <= j)
-    {   
-        while (A[i] < pivot)
-        {
+    while (1) {
+        // Increment i while A[i] < pivot
+        do {
             i++;
-        }
+        } while (A[i] < pivot);
 
-        while (A[j] > pivot)
-        {
+        // Decrement j while A[j] > pivot
+        do {
             j--;
-        }
+        } while (A[j] > pivot);
 
-        if (i <= j){ Swap(A, i, j); }
+        // If two pointers met
+        if (i >= j)
+            return j;
+
+        // Swap elements at i and j
+        Swap(A, i, j);
     }
+}
 
-    Swap(A, l, j);
-    return j;
+int hoarePartitionRight(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1) {
+        // Increment i while arr[i] < pivot
+        do {
+            i++;
+        } while (arr[i] < pivot);
+
+        // Decrement j while arr[j] > pivot
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        // If two pointers met
+        if (i >= j)
+            return j;
+
+        // Swap elements at i and j
+        Swap(A, i, j);
+    }
 }
 
 void quickSort(int* A, int l, int r)
 {
     if (l < r)
     {
-        int m = hoarePartition(A, l, r);
+        int m = hoarePartitionRight(A, l, r);
         quickSort(A, l, m);
         quickSort(A, m+1, r);
     }
