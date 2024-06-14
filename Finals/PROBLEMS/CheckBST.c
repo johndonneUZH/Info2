a9b14b40502c69c6b52bb5a208f456ddd5f806f7
Adam Matteo Iulian <144601577+johndonneUZH@users.noreply.github.com>
2024-06-14 12:44:03 +0200

    Update CheckBST.c

--- Finals/PROBLEMS/CheckBST.c
+++ Finals/PROBLEMS/CheckBST.c
@@ -7,10 +7,16 @@ struct node {
 struct node* root;
 Implement a function isBST(struct node* root) that checks if a tree is a BST*/
 
-bool isBST(node* root)
+bool BSThelper(struct TreeNode* root, int min, int max)
 {
     if (root == NULL) return true;
-    if (root->left && root->val < root->left->val) return false;
-    if (root->right && root->val > root->right->val) return false;
-    return isBST(root->left) && isBST(root->right)
+
+    if (root->val <= min || root->val >= max) return false;
+
+    return BSThelper(root->left, min, root->val) && BSThelper(root->right, root->val, max);
+}
+
+
+bool isValidBST(struct TreeNode* root) {
+    return BSThelper(root, INT_MIN, INT_MAX);
 }
